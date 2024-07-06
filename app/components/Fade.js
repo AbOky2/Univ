ce14a12d50488743914fddd5c2915a4b0d1effb1
AbOky2 <issaokiabderamane@gmail.com>
2024-07-06 15:15:51 +0100

    mise a jour

--- app/components/Fade.js
+++ app/components/Fade.js
@@ -7,9 +7,11 @@ export function FadeDown() {
     show: { opacity: 1, y: 0, transition: { type: "spring" } },
   };
   return (
+    <div>
     <motion.div
-    className=" h-screen bg-[#225c8b] items-center flex flex-col justify-center "
-      initial="hidden"
+    className=" h-screen  items-center flex flex-col justify-center "
+    style={{ backgroundImage: "url('/etudiants.jpg')" }}
+    initial="hidden"
       animate="show"
       viewport={{ once: true }}
       variants={{
@@ -40,5 +42,7 @@ export function FadeDown() {
         If you&apos;re seeing this, thank you for trying my project out! - C.J.A
       </motion.div>
     </motion.div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#225c8b" fill-opacity="1" d="M0,128L60,154.7C120,181,240,235,360,261.3C480,288,600,288,720,266.7C840,245,960,203,1080,202.7C1200,203,1320,245,1380,266.7L1440,288L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+    </div>
   );
 }
